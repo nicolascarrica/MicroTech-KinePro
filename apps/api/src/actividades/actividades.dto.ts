a89d811d73b258d322dc87892e7905a39670c6ae
Nicolas Carrica <102767489+nicolascarrica@users.noreply.github.com>
2026-05-19 08:08:42 -0300

    Feature/actividades (#1)
    * fix(api): comentar ignoreDeprecations invalido en tsconfig
    
    * feat(actividades): crear actividad (HU crear actividad)
    
    * chore: ignorar archivos tsbuildinfo de typescript
    
    * feat(actividades):agregar,  modificar y eliminar actividad (HUs agregar modificar y eliminar)

---
+++ apps/api/src/actividades/actividades.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsNotEmpty, MaxLength } from 'class-validator';
+
+export class CreateActividadDto {
+  @IsString()
+  @IsNotEmpty({ message: 'El nombre de la actividad es obligatorio' })
+  @MaxLength(50, { message: 'El nombre no puede superar los 50 caracteres' })
+  nombre!: string;
+}
+
+export class ModificarActividadDto {
+  @IsString()
+  @IsNotEmpty({ message: 'El nombre de la actividad es obligatorio' })
+  @MaxLength(50, { message: 'El nombre no puede superar los 50 caracteres' })
+  nombre!: string;
+}
--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { HealthModule } from './health/health.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ActividadesModule } from './actividades/actividades.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { PrismaModule } from './prisma/prisma.module';
     HealthModule,
     PrismaModule,
     UsuariosModule,
+    ActividadesModule,
   ],
 })
 export class AppModule {}
